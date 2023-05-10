032142d76a86980abdfef07a91c0d27bfb96a178
Adithyakm <captainadithya26@gmail.com>
2023-05-10 18:42:28 +0530

    committing the changes

--- room/src/main/java/room/CodemindersSocketIoServer.java
+++ room/src/main/java/room/CodemindersSocketIoServer.java
@@ -18,7 +18,7 @@ import com.codeminders.socketio.server.Namespace;
 import com.codeminders.socketio.server.Socket;
 import com.codeminders.socketio.server.SocketIOServlet;
 
-@WebServlet(urlPatterns = "/socket.io/*",asyncSupported=true)
+//@WebServlet(urlPatterns = "/socket.io/*",asyncSupported=true)
 public class CodemindersSocketIoServer extends SocketIOServlet {
 
 	@Override
@@ -37,9 +37,46 @@ public class CodemindersSocketIoServer extends SocketIOServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//super.doGet(req, resp);
+//		HttpSession session = req.getSession();
+//		System.out.println("Session details : "+session.getId());
+//		Namespace namespace = namespace("/");
+//		//Socket socket = namespace.createSocket(session);
+//		System.out.println("namespace created : "+namespace.getId());
+//		namespace.on(new ConnectionListener() {
+//
+//			@Override
+//			public void onConnect(final Socket socket) throws ConnectionException {
+//				// TODO Auto-generated method stub
+//				System.out.println("Client connected successfully");
+//				System.out.println("connection details : ");
+//				System.out.println(socket.getSession().getHttpSession().getId());
+//				socket.on("HelloWorld", new EventListener() {
+//
+//					@Override
+//					public Object onEvent(String name, Object[] args, boolean ackRequested) {
+//						// TODO Auto-generated method stub
+//						System.out.println(name);
+//						System.out.println(args[0]);
+//						try {
+//							socket.emit("HelloReturned", "Hello World");
+//						} catch (SocketIOException e) {
+//							// TODO Auto-generated catch block
+//							e.printStackTrace();
+//						}
+//						return ackRequested;
+//					}
+//					
+//				});
+//			}
+//			
+//		});
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		HttpSession session = req.getSession();
 		System.out.println("Session details : "+session.getId());
-		
 		Namespace namespace = namespace("/");
 		//Socket socket = namespace.createSocket(session);
 		System.out.println("namespace created : "+namespace.getId());
@@ -68,17 +105,18 @@ public class CodemindersSocketIoServer extends SocketIOServlet {
 					}
 					
 				});
+				
 			}
 			
 		});
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
-	
+//	@Override
+//	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		super.doPost(req, resp);
+//	}
+//	
 	
 
 }
